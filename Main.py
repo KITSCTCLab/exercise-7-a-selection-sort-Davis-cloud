b66755c996cec4f4c841eaeb774d51f18af40506
URK21CS2009 Davis Alex <109649381+Davis-cloud@users.noreply.github.com>
2022-10-12 09:31:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from typing import List
 
 def selectionSort(array, size) -> List[int]:  
   
@@ -9,12 +10,3 @@ def selectionSort(array, size) -> List[int]:
     array[indexo], array[small_index] = array[small_index], array[indexo]
   return array     
 
-# Do not change the following code
-input_data = input()
-data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-print(selectionSort(data, len(data)))
